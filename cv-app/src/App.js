2fde0f341700a7a9c25f30f601e89666d4124df3
Anano Topuria <anano.topuriaa@gmail.com>
2023-10-12 15:40:03 +0400

    implement pages and routes

--- cv-app/src/App.js
+++ cv-app/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'; 
+import Inner from './pages/Inner';
+import PageNotFound from "./pages/PageNotFound";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router>
+    //   <Routes>
+    //     
+    //     {/* Add routes for other components if needed */}
+    //   </Routes>
+    // </Router>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/inner" element={<Inner />} />
+      <Route path="*" element={<PageNotFound/>} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ cv-app/src/pages/Inner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Inner() {
+  return (
+    <div>
+      <h1>Inner Page</h1>
+    </div>
+  );
+}
+
+export default Inner;
