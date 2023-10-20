835c4006ef262448252e9cddfe75e3196953d1c4
Anano Topuria <anano.topuriaa@gmail.com>
2023-10-20 13:15:29 +0400

    implement smooth scrolling for navigation

--- cv-app/src/components/MainContent.js
+++ cv-app/src/components/MainContent.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import Box from './Box';
-import Timeline from '../components/Timeline';
-import Expertise from '../components/Expertise';
-import Portfolio from '../components/Portfolio';
-import Feedback from '../components/Feedback';
-import userimage from '../assets/images/userimage.png';
-import Address from '../components/Address';
-
+import React from "react";
+import Box from "./Box";
+import Timeline from "../components/Timeline";
+import Expertise from "../components/Expertise";
+import Portfolio from "../components/Portfolio";
+import Feedback from "../components/Feedback";
+import userimage from "../assets/images/userimage.png";
+import Address from "../components/Address";
 
 const MainContent = ({ isOpen }) => {
   const mainContentClasses = `transition-margin ${
@@ -16,83 +15,97 @@ const MainContent = ({ isOpen }) => {
   }`;
   return (
     <div className={mainContentClasses}>
-      <Box title="About me" content="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque" />
-      <Box title="Education"/>
-      <Timeline
+      <div id="about">
+        <Box
+          title="About me"
+          content="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque"
+        />
+      </div>
+      <div id="education">
+        <Box title="Education" />
+        <Timeline
           id="education"
           data={[
             {
               date: 2016,
-              title: 'Bachelors degree, law',
-              text: 'Caucasus University',
+              title: "Bachelors degree, law",
+              text: "Caucasus University",
             },
             {
               date: 2019,
-              title: 'Masters degree, law',
-              text: 'Georgian Technical University',
+              title: "Masters degree, law",
+              text: "Georgian Technical University",
             },
             {
               date: 2023,
-              title: 'Learn Digital Platform - EPAM',
-              text: 'EPAM',
+              title: "Learn Digital Platform - EPAM",
+              text: "EPAM",
             },
           ]}
         />
+      </div>
+      <div id="experience">
         <Box title="Experience" />
         <Expertise
           id="experience"
           data={[
             {
-              date: '2020 – 2022',
+              date: "2020 – 2022",
               info: {
-                company: 'Adjara group',
-                job: 'UX/UI Designer',
+                company: "Adjara group",
+                job: "UX/UI Designer",
                 description:
-                  'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+                  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.",
               },
             },
             {
-              date: '2023',
+              date: "2023",
               info: {
-                company: 'Google',
-                job: 'Front-End Developer',
+                company: "Google",
+                job: "Front-End Developer",
                 description:
-                  'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+                  "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.",
               },
             },
           ]}
         />
-      <Box title="Skills" />
-      <Box title="Portfolio" />
-      <Portfolio />
-      <Box title="Contact" />
-      <Address id="contacts"/>
-      <Box title="Feedback" />
-      <Feedback
+      </div>
+      <div id="skills">
+        <Box title="Skills" />
+      </div>
+      <div id="portfolio">
+        <Box title="Portfolio" />
+        <Portfolio />
+      </div>
+      <div id="contacts">
+        <Box title="Contact" />
+        <Address />
+      </div>
+      <div id="feedback">
+        <Box title="Feedback" />
+        <Feedback
           data={[
             {
               feedback:
-                'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor, Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor, Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  ',
+                "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor, Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor, Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  ",
               reporter: {
                 photoUrl: userimage,
-                name: 'John Doe Programer',
-                citeUrl: 'https://www.citeexample.com',
+                name: "John Doe Programer",
+                citeUrl: "https://www.citeexample.com",
               },
             },
             {
               feedback:
-                'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor, Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor, Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  ',
+                "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor, Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor, Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  ",
               reporter: {
                 photoUrl: userimage,
-                name: 'John Doe Programer',
-                citeUrl: 'https://www.citeexample.com',
+                name: "John Doe Programer",
+                citeUrl: "https://www.citeexample.com",
               },
             },
           ]}
-          id="feedback"
-          
         />
-
+      </div>
     </div>
   );
 };
--- cv-app/src/components/Nav.js
+++ cv-app/src/components/Nav.js
@@ -10,55 +10,99 @@ import {
   faComment,
   faChevronLeft,
 } from "@fortawesome/free-solid-svg-icons";
-import Button from "./Button";
+import Button from "./Button"; 
+import { Link as ScrollLink } from "react-scroll";
 
-const Nav = () => {
+function Nav() {
   return (
-    <nav className="p-8">
-      <ul className="flex flex-col gap-7 sm:w-32 md:w-32">
-        <li className="hidden sm:block ">
-          <div className="cursor-pointer hover:text-[#26c27e]">
-            <FontAwesomeIcon icon={faUser} />
-            <span className="p-4">About Me</span>
-          </div>
-        </li>
-        <li className="hidden sm:block">
-          <div className="cursor-pointer hover:text-[#26c27e]">
-            <FontAwesomeIcon icon={faGraduationCap} />
-            <span className="p-4">Education</span>
-          </div>
-        </li>
-        <li className="hidden sm:block">
-          <div className="cursor-pointer hover:text-[#26c27e]">
-            <FontAwesomeIcon icon={faPencil} />
-            <span className="p-3">Experience</span>
-          </div>
-        </li>
-        <li className="hidden sm:block">
-          <div className="cursor-pointer hover:text-[#26c27e]">
-            <FontAwesomeIcon icon={faMagicWandSparkles} />
-            <span className="p-4">Skills</span>
-          </div>
-        </li>
-        <li className="hidden sm:block">
-          <div className="cursor-pointer hover:text-[#26c27e]">
-            <FontAwesomeIcon icon={faSuitcase} />
-            <span className="p-4">Portfolio</span>
-          </div>
-        </li>
-        <li className="hidden sm:block">
-          <div className="cursor-pointer hover:text-[#26c27e]">
-            <FontAwesomeIcon icon={faAddressBook} />
-            <span className="p-4">Contacts</span>
-          </div>
-        </li>
-        <li className="hidden sm:block">
-          <div className="cursor-pointer hover:text-[#26c27e]">
-            <FontAwesomeIcon icon={faComment} />
-            <span className="p-4">Feedback</span>
-          </div>
-        </li>
-        <li className="hidden sm:block">
+    <div>
+      <div className="p-8">
+        <ul className="p- flex flex-col gap-7 sm:w-32 md:w-32">
+          <li className="hidden sm:block">
+            <ScrollLink
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faUser} />
+              <span className="p-4">About Me</span>
+            </ScrollLink>
+          </li>
+          <li className="hidden sm:block">
+            <ScrollLink
+              to="education"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faGraduationCap} />
+              <span className="p-4">Education</span>
+            </ScrollLink>
+          </li>
+          <li className="hidden sm:block">
+            <ScrollLink
+              to="experience"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faPencil} />
+              <span className="p-3">Experience</span>
+            </ScrollLink>
+          </li>
+          <li className="hidden sm:block">
+            <ScrollLink
+              to="skills"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faMagicWandSparkles} />
+              <span className="p-4">Skills</span>
+            </ScrollLink>
+          </li>
+          <li className="hidden sm:block">
+            <ScrollLink
+              to="portfolio"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faSuitcase} />
+              <span className="p-4">Portfolio</span>
+            </ScrollLink>
+          </li>
+          <li className="hidden sm:block">
+            <ScrollLink
+              to="contacts"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faAddressBook} />
+              <span className="p-4">Contacts</span>
+            </ScrollLink>
+          </li>
+          <li className="hidden sm:block">
+            <ScrollLink
+              to="feedback"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faComment} />
+              <span className="p-4">Feedback</span>
+            </ScrollLink>
+          </li>
+          <li className="hidden sm:block">
           <Link
             to="/"
             className="absolute bottom-8 left-5 md:w-[9rem] lg:w-[10rem]"
@@ -69,61 +113,99 @@ const Nav = () => {
             </Button>
           </Link>
         </li>
-      </ul>
-      <ul className="sm:hidden">
-        <li className="nav-item">
-          <FontAwesomeIcon
-            icon={faUser}
-            className="cursor-pointer py-2 hover:text-[#26c27e]"
-          />
-        </li>
-        <li className="nav-item">
-          <FontAwesomeIcon
-            icon={faGraduationCap}
-            className="cursor-pointer py-2 hover:text-[#26c27e]"
-          />
-        </li>
-        <li className="nav-item">
-          <FontAwesomeIcon
-            icon={faPencil}
-            className="cursor-pointer py-2 hover:text-[#26c27e]"
-          />
-        </li>
-        <li className="nav-item">
-          <FontAwesomeIcon
-            icon={faMagicWandSparkles}
-            className="cursor-pointer py-2 hover:text-[#26c27e]"
-          />
-        </li>
-        <li className="nav-item">
-          <FontAwesomeIcon
-            icon={faSuitcase}
-            className="cursor-pointer py-2 hover:text-[#26c27e]"
-          />
-        </li>
-        <li className="nav-item">
-          <FontAwesomeIcon
-            icon={faAddressBook}
-            className="cursor-pointer py-2 hover:text-[#26c27e]"
-          />
-        </li>
-        <li className="nav-item">
-          <FontAwesomeIcon
-            icon={faComment}
-            className="cursor-pointer py-2 hover:text-[#26c27e]"
-          />
-        </li>
-        <li className="nav-item absolute bottom-8 left-5">
+        </ul>
+      </div>
+      <div>
+        <ul className="sm:hidden flex flex-col gap-4 items-center">
+          <li className="nav-item">
+            <ScrollLink
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer  hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faUser} />
+            </ScrollLink>
+          </li>
+          <li className="nav-item">
+            <ScrollLink
+              to="education"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faGraduationCap} />
+            </ScrollLink>
+          </li>
+          <li className="nav-item">
+            <ScrollLink
+              to="experience"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faPencil} />
+            </ScrollLink>
+          </li>
+          <li className="nav-item">
+            <ScrollLink
+              to="skills"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faMagicWandSparkles} />
+            </ScrollLink>
+          </li>
+          <li className="nav-item">
+            <ScrollLink
+              to="portfolio"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faSuitcase} />
+            </ScrollLink>
+          </li>
+          <li className="nav-item">
+            <ScrollLink
+              to="contacts"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faAddressBook} />
+            </ScrollLink>
+          </li>
+          <li className="nav-item">
+            <ScrollLink
+              to="feedback"
+              spy={true}
+              smooth={true}
+              duration={800}
+              className="cursor-pointer hover:text-[#26c27e]"
+            >
+              <FontAwesomeIcon icon={faComment} />
+            </ScrollLink>
+          </li>
+          <li className="nav-item absolute bottom-8 left-5">
           <Link
-            to="/"
-            className="cursor-pointer rounded-lg bg-[#26c17e] px-4 py-2 text-[#fff]"
-          >
-            <FontAwesomeIcon icon={faChevronLeft} />
-          </Link>
-        </li>
-      </ul>
-    </nav>
+             to="/"
+             className="cursor-pointer rounded-lg bg-[#26c17e] px-4 py-2 text-[#fff]"
+           >
+             <FontAwesomeIcon icon={faChevronLeft} />
+           </Link>
+         </li> 
+        </ul>
+      </div>
+    </div>
   );
-};
+}
 
 export default Nav;
